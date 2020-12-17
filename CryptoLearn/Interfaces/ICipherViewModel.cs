14644d6231ade342fa17e4e8ef8f367a37f3004a
Asilzhan <asilzhan.asa.j@gmail.com>
2020-12-17 13:51:04 +0600

    Добавлены интерфейсы

---
+++ CryptoLearn/Interfaces/ICipherViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace CryptoLearn.Interfaces
+{
+	public interface ICipherViewModel
+	{
+		public string PlainText { get; set; }
+		public string Key { get; set; }
+		public string CipherText { get; set; }
+		public string EncryptData(string text, string key);
+		public string DecryptData(string text, string key);
+	}
+	public interface IVigenereViewModel
+	{
+		public string PlainText { get; set; }
+		public string Alphabet { get; set; }
+		public string CipherText { get; set; }
+		
+		public string Key { get; set; }
+		
+		public string EncryptData(string text, string key);
+		public string DecryptData(string text, string key);
+	}
+}
--- CryptoLearn/ViewModels/MainWIndowViewModel.cs
+++ CryptoLearn/ViewModels/MainWIndowViewModel.cs
@@ -3,12 +3,19 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using CryptoLearn.Annotations;
 using CryptoLearn.Helper;
+using CryptoLearn.Interfaces;
 
 namespace CryptoLearn.ViewModels
 {
     internal class MainWindowViewModel : INotifyPropertyChanged
     {
-
+        public ICeaserViewModel CeaserViewModel { get; set; }
+        public IRsaViewModel RsaViewModel { get; set; }
+        public IVigenereViewModel VigenereViewModel { get; set; }
+        public ICipherViewModel DesViewModel { get; set; }
+        public ICipherViewModel AesViewModel { get; set; }
+        public ICipherViewModel GostViewModel { get; set; }
+        
         #region INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
 
