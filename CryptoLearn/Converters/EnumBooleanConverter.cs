829f067a704e69ab1c9621dbbb03c5239a081128
yergesh <54017134+yergesh@users.noreply.github.com>
2020-12-17 17:54:45 +0600

    convert

---
+++ CryptoLearn/Converters/EnumBooleanConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CryptoLearn.Converters
+{
+    public class EnumBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var ParameterString = parameter as string;
+            if (ParameterString == null)
+                return DependencyProperty.UnsetValue;
+
+            if (Enum.IsDefined(value.GetType(), value) == false)
+                return DependencyProperty.UnsetValue;
+
+            object paramvalue = Enum.Parse(value.GetType(), ParameterString);
+            return paramvalue.Equals(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var ParameterString = parameter as string;
+            var valueAsBool = (bool)value;
+
+            if (ParameterString == null || !valueAsBool)
+            {
+                try
+                {
+                    return Enum.Parse(targetType, "0");
+                }
+                catch (Exception)
+                {
+                    return DependencyProperty.UnsetValue;
+                }
+            }
+            return Enum.Parse(targetType, ParameterString);
+        }
+    }
+}
