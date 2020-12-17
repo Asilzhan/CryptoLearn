17650a8c04ec0ef647252ed3b20ae0468990657d
Asilzhan <asilzhan.asa.j@gmail.com>
2020-12-17 13:51:04 +0600

    Добавлены интерфейсы

---
+++ CryptoLearn/Interfaces/ICipherViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CryptoLearn.Interfaces
+{
+	public interface ICipherViewModel
+	{
+		public string PlainText { get; set; }
+		public string Key { get; set; }
+		public string CipherText { get; set; }
+		public string EncryptData(string text, string key);
+		public string DecryptData(string text, string key);
+	}
+}
---
+++ CryptoLearn/Interfaces/IVigenereViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CryptoLearn.Interfaces
+{
+	public interface IVigenereViewModel
+	{
+		public string PlainText { get; set; }
+		public string Alphabet { get; set; }
+		public string CipherText { get; set; }
+		
+		public string Key { get; set; }
+		
+		public string EncryptData(string text, string key);
+		public string DecryptData(string text, string key);
+	}
+}
