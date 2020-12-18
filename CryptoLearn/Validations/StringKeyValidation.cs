64cc3b8c7bf27a194b26bf7e32238de265b64eca
yergesh <54017134+yergesh@users.noreply.github.com>
2020-12-18 21:14:09 +0600

    Цезарь бітті

---
+++ CryptoLearn/Validations/StringKeyValidation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Controls;
+using CryptoLearn.Models;
+
+namespace CryptoLearn.Validations
+{
+    public class StringKeyValidation : ValidationRule
+    {
+        public string Alphabet { get; set; }
+        
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string key = value?.ToString() ?? "";
+            foreach (var t in key)
+            {
+                if (Alphabet.IndexOf(t, StringComparison.OrdinalIgnoreCase) == -1)
+                    return new ValidationResult(false, "Алфавит тілі дұрыс емес!");
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
